646350150271dd5096bc171ddded6d85eedb94b2
termux <termux@termux.com>
2021-01-10 17:56:16 +0700

    setup

--- ci_build.bash
+++ ci_build.bash
@@ -3,10 +3,10 @@ set -e
 
 # Install embedded_device_map
 chmod 755 secrets/embedded_device_map
-ln -s secrets/embedded_device_map /usr/bin/embedded_device_map
+cp secrets/embedded_device_map /usr/bin/
 
 mkdir -p /usr/local/standalone/firmware
-ln -s secrets/device_map.db /usr/local/standalone/firmware/
+cp secrets/device_map.db /usr/local/standalone/firmware/
 
 # setup env
 
